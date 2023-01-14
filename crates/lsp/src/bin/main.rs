4233c6564a028b726800c6ad272b0dd65e152b6d
Jamie Cunliffe <Jamie.Cunliffe@outlook.com>
2023-01-14 20:05:01 +0000

    Switch from jemalloc to mimalloc.

--- crates/lsp/src/bin/main.rs
+++ crates/lsp/src/bin/main.rs
@@ -1,6 +1,5 @@
-#[cfg(not(target_env = "msvc"))]
 #[global_allocator]
-static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 #[macro_use]
 extern crate log;
